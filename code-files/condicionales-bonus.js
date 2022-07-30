1aa583d943ff2920cbe0a7cac4d13ba55547e338
Michael.Rdz <91229705+michaelrdz@users.noreply.github.com>
2022-07-29 21:12:55 -0500

    carga código JS
    carga código JS

--- code-files/condicionales-bonus.js
+++ code-files/condicionales-bonus.js
@@ -1,1 +1,16 @@
+/*💡 Bonus: si ya eres una experta o experto en el lenguaje, te desafío a comentar cómo replicar 
+este comportamiento con arrays u objetos y un solo condicional. 😏 */
+
 //Solución utilizando un objeto que guarde los mensajes posibles.
+
+const tipoDeSuscripcion = "Basic";
+
+let cuentas = {
+    "Free" : "Solo puedes tomar los cursos gratis",
+    "Basic" : "Puedes tomar casi todos los cursos de Platzi durante un mes",
+    "Expert" : "Puedes tomar casi todos los cursos de Platzi durante un año",
+}
+
+if( cuentas[tipoDeSuscripcion] != undefined){
+    console.log(cuentas[tipoDeSuscripcion]);
+}
