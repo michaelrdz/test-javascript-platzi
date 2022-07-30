d58fa6b46937a4c8fe7e244e24cde101267b69e6
Michael.Rdz <91229705+michaelrdz@users.noreply.github.com>
2022-07-29 21:09:59 -0500

    Carga código JS
    Carga código JS

---
+++ code-files/variables.js
@@ -0,0 +1,21 @@
+/*
+let name = "Michael";
+let Lastname = "Rodríguez Medina";
+let user = "michaelrdz";
+let age = 29;
+let email = "michaelrdz.me@gmail.com";
+let mayor = true;
+let saving = 65000;
+let debt = 12000;
+*/
+
+//Nombre completo (nombre y apellido):
+    let name = "Michael";
+    let lastName = "Rodríguez Medina";
+    console.log("Nombre completo: "+name+" "+lastname);
+
+//Dinero real (dinero ahorrado menos deudas)
+    let saving = 65000;
+    let debt = 12000;
+    let real = saving - debt; 
+    console.log("Dinero real: "+real);
