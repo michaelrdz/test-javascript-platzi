a6dc3f42670765cbfc8a166891369a86b1fb1d61
Michael.Rdz <91229705+michaelrdz@users.noreply.github.com>
2022-07-29 21:11:45 -0500

    carga código JS
    carga código JS

--- code-files/condicionales.js
+++ code-files/condicionales.js
@@ -1,3 +1,21 @@
+//Replica el comportamiento del siguiente código que usa la sentencia switch utilizando if, else y else if: const tipoDeSuscripcion = "Basic";
+
+/*switch (tipoDeSuscripcion) {
+    case "Free":
+        console.log("Solo puedes tomar los cursos gratis");
+        break;
+    case "Basic":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un mes");
+        break;
+    case "Expert":
+        console.log("Puedes tomar casi todos los cursos de Platzi durante un año");
+        break;
+    case "ExpertPlus":
+        console.log("Tú y alguien más pueden tomar TODOS los cursos de Platzi durante un año");
+        break;
+}*/
+
+
 const tipoDeSuscripcion = "Basic";
 
 //Replica el comportamiento del siguiente código que usa la sentencia switch utilizando if, else y else if: const tipoDeSuscripcion = "Basic";
